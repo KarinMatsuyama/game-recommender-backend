562874d5f15be5698409923aa3b5be4a2f31bd0b
KarinMatsuyama <m.karin2@gmail.com>
2019-07-29 10:06:08 -0500

    changed permission settings

--- game_ratings_recommender/settings.py
+++ game_ratings_recommender/settings.py
@@ -47,8 +47,8 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        # 'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
